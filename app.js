e1ef237a38b45b0171a92dcb4e6a6907f6ecfcb8
taggaddaaaa <scaizerg@student.42.fr>
2019-05-05 22:16:34 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+new Vue({
+	el: '#app',
+	
+	data: {
+		currencies: {}
+	},
+	
+	mounted() {
+		this.getCurrencies();
+		
+		},
+	
+	
+	methods: {
+		/**
+		 * Get currencies from API (https://free.currencyconverterapi.com/)
+		 * We store data in localStorage to avoid consuming the API each time
+		 * Otherwise, if there is no currencies value in localStorage, we call the API
+		 *
+		 * @return collection of currencies
+		 */
+		getCurrencies() {
+			const currencies = localStorage.getItem('currencies');
+			
+			if (currencies) {
+				this.currencies = localStorage.getItem('currencies');
+				
+				return;
+			}
+			
+			axios.get('https://free.currconv.com/api/v7/currencies?apiKey=63ab6589eff9bc56bc24')
+			.then(response =>{
+				//console.log('response: ', response);
+				this.currencies = response.data.results;
+				localStorage.setItem('currencies', JSON.stringify(response.data.results));
+			});
+		},
+		
+	},
+});
